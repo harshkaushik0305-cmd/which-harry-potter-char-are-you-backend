dcd52d38782599318d0e5519f25c32a91ce08cb4
Yuvraj Kaushik <harsh.kaushik0305@gmail.com>
2026-01-09 19:07:51 +0530

    Delete routes directory

--- routes/charachter.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getCharachter } from '../controllers/charachter.controller.js';
-
-const router = express.Router();
-
-router.post('/get-charachter', getCharachter)
-
-export default router;
--- routes/healthCheck.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { healthCheck } from '../controllers/healthCheck.controller.js';
-
-const router = express.Router();
-
-router.get('/healthCheck',healthCheck)
-
-export default router
